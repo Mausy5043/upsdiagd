57df936e5ae37ed7ca7c2980fa4f27489b01f41b
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-09-17 14:55:25 +0200

    update

--- mkgraphs.sh
+++ mkgraphs.sh
@@ -8,15 +8,17 @@ UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
 UTCSECONDS=$(date -d "$UTC" +%s)
 UTCOFFSET=$((LOCALSECONDS - UTCSECONDS))
 
-pushd "$HOME/upsdiagd" >/dev/null
+pushd "$HOME/upsdiagd" >/dev/null || exit 1
   if [ $(wc -l < /tmp/upsdiagd/mysql/upsd.csv) -gt 5 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./ups13.gp &
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./ups14.gp &
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./ups15.gp &
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./ups16.gp &
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./ups17.gp &
+    echo -n "UPS13"
+    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/ups13.gp
+    echo -n "UPS14"
+    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/ups14.gp
+    echo -n "UPS15"
+    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/ups15.gp
+    echo -n "UPS16"
+    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/ups16.gp
+    echo -n "UPS17"
+    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/ups17.gp
   fi
-
-  wait
-
 popd >/dev/null

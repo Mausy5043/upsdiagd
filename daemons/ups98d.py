f10bfd1c06cecfcbc274ff92ff03a1bbba4900a7
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-05-06 13:47:04 +0200

    add parameter

--- daemons/ups98d.py
+++ daemons/ups98d.py
@@ -49,7 +49,7 @@ class MyDaemon(Daemon):
     sqldata.get(sqldata.d_cmd)
     sqldata.get(sqldata.w_cmd)
     sqldata.get(sqldata.y_cmd)
-    if (trendgraph.draw() == 0):
+    if (trendgraph.draw(trendgraph.command) == 0):
       upload_page(scriptname)
     while True:
       try:

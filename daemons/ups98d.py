6b3c37695708900ab49f7f6bea85a14a02942c55
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-05-06 13:44:44 +0200

    drawing

--- daemons/ups98d.py
+++ daemons/ups98d.py
@@ -49,7 +49,7 @@ class MyDaemon(Daemon):
     sqldata.get(sqldata.d_cmd)
     sqldata.get(sqldata.w_cmd)
     sqldata.get(sqldata.y_cmd)
-    if (trendgraph.make() == 0):
+    if (trendgraph.draw() == 0):
       upload_page(scriptname)
     while True:
       try:
@@ -140,14 +140,19 @@ class Graph(object):
     self.timer      = time.time() + rnd(60, self.updatetime)
     self.command    = self.home + '/' + MYAPP + '/mkgraphs.sh'
 
+  def draw(self, cmnd):
+    """Draw the graphs"""
+    mf.syslog_trace("...:  {0}".format(cmnd), False, DEBUG)
+    result = subprocess.call(cmnd)
+    return result
+
   def make(self):
     t0 = time.time()
     if t0 >= self.timer:
-      mf.syslog_trace("...:  {0}".format(self.command), False, DEBUG)
+      result = self.draw(self.command)
       t1 = time.time()
       self.timer = t1 + self.updatetime + rnd(-60, 60)
-      return subprocess.call(self.command)
-    return 1
+    return result
 
 def do_stuff(flock, script):
   # wait 4 seconds for processes to finish

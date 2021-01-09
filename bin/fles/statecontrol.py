0d750476b363df8a4635aeb2a6d67a120bcec4e9
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2021-01-09 21:12:31 +0100

    website update

--- bin/fles/statecontrol.py
+++ bin/fles/statecontrol.py
@@ -15,50 +15,29 @@ def state():
     global KRAT
     if flask.request.method == 'POST':
         pass
-        # if flask.request.form.get('Control') == 'Instellingen...':
-        #     print("Requesting access to Controller")
-        #     return flask.redirect(flask.url_for('controller'))
 
     if flask.request.method == 'GET':
         pass
 
-    th_img = "".join(["data:image/png;base64,",
-                      str(base64.b64encode(open("/tmp/aircon/site/img/pastday_TH.png",
+    chg_img = "".join(["data:image/png;base64,",
+                      str(base64.b64encode(open("/tmp/upsdiagd/site/img/pastday_CHG.png",
                                                 "rb").read()))[2:-1]]
                      )
-    m_img = "".join(["data:image/png;base64,",
-                     str(base64.b64encode(open("/tmp/aircon/site/img/pastday_M.png",
-                                               "rb").read()))[2:-1]]
-                    )
-    p_img = "".join(["data:image/png;base64,",
-                     str(base64.b64encode(open("/tmp/aircon/site/img/pastday_P.png",
+    run_img = "".join(["data:image/png;base64,",
+                     str(base64.b64encode(open("/tmp/upsdiagd/site/img/pastday_RUN.png",
                                                "rb").read()))[2:-1]]
                     )
     v_img = "".join(["data:image/png;base64,",
-                     str(base64.b64encode(open("/tmp/aircon/site/img/pastday_VOC.png",
+                     str(base64.b64encode(open("/tmp/upsdiagd/site/img/pastday_V.png",
                                                "rb").read()))[2:-1]]
                     )
-    c_img = "".join(["data:image/png;base64,",
-                     str(base64.b64encode(open("/tmp/aircon/site/img/pastday_CO2.png",
-                                               "rb").read()))[2:-1]]
-                    )
-    f_img = "".join(["data:image/png;base64,",
-                     str(base64.b64encode(open("/tmp/aircon/site/img/pastday_F.png",
-                                               "rb").read()))[2:-1]]
-                    )
-    gld = KRAT.get_latest_data('volt_bat, load_ups, charge_bat')
+    # gld = KRAT.get_latest_data('volt_bat, load_ups, charge_bat')
     return flask.render_template('state.html',
-                                 room=f'{KRAT.ROOM_ID}',
-                                 volt_in=f"{gld[0]:.1f} \u00B0C",
-                                 volt_in_img=th_img,
-                                 load_ups=f"{gld[1]:.1f} %",
-                                 moisture=f"{mst:.1f} g/m\u00B3",
-                                 moisture_img=m_img,
-                                 charge_bat=f"{gld[2]:.0f} mbara",
-                                 charge_bat_img=p_img,
-                                 voc=f"{gld[3]:.0f} ppb",
-                                 voc_img=v_img,
-                                 co2=f"{gld[4]:.0f} ppm",
-                                 co2_img=c_img,
-                                 freq_img=f_img
+                                 volt_in="n/a",  # f"{gld[0]:.1f} \u00B0C",
+                                 volt_in_img=v_img,
+                                 load_ups="n/a",  # f"{gld[1]:.1f} %",
+                                 charge_bat="n/a",  # f"{gld[2]:.0f} mbara",
+                                 charge_bat_img=chg_img,
+                                 run="n/a",  # f"{gld[3]:.0f} ppb",
+                                 run_img=run_img
                                  )

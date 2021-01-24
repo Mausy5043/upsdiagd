4c4636894bd39e020086b074cb985d2214c0acb3
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2021-01-24 20:37:00 +0100

    -x

--- install.sh
+++ install.sh
@@ -93,8 +93,9 @@ pushd "${HERE}" || exit 1
     git config core.fileMode false
     # set the branch
     if [ ! -e "${HOME}/.${app_name}.branch" ]; then
-        echo "master" >"${HOME}/.${app_name}.branch"
+        echo "v4" >"${HOME}/.${app_name}.branch"
     fi
+    chmod -x ./services/*
 
     # Recover the database from the server
     ./bin/bakrecdb.sh --install

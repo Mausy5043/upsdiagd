bc67b2daafda176c59a6a62f4b6d94ceec73417f
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2021-05-27 10:45:43 +0200

    recode graphing

--- bin/trend.py
+++ bin/trend.py
@@ -78,201 +78,98 @@ def y_ax_limits(data_set, accuracy):
     return [lo_limit, hi_limit]
 
 
-def plot_graph(output_file, data_tuple, plot_title):
+def plot_graph(output_file, data_frame, plot_title):
     """
     Create graphs
     """
-    data_lbls = data_tuple[0]
-    volt_in = data_tuple[1][:, 0]
-    volt_in_min = data_tuple[1][:, 1]
-    volt_in_max = data_tuple[1][:, 2]
-    charge_bat = data_tuple[2][:, 0]
-    charge_bat_min = data_tuple[2][:, 1]
-    charge_bat_max = data_tuple[2][:, 2]
-    load_ups = data_tuple[3][:, 0]
-    load_ups_min = data_tuple[3][:, 1]
-    load_ups_max = data_tuple[3][:, 2]
-    runtime_bat = data_tuple[4][:, 0]
-    runtime_bat_min = data_tuple[4][:, 1]
-    runtime_bat_max = data_tuple[4][:, 2]
-    volt_bat = data_tuple[5][:, 0]
-    volt_bat_min = data_tuple[5][:, 1]
-    volt_bat_max = data_tuple[5][:, 2]
 
-    """
-    --- Start debugging:
-    np.set_printoptions(precision=3)
-    print("data_lbls   : ", np.size(data_lbls), data_lbls[-5:])
-    print(" ")
-    print("Line    [V] : ", np.size(volt_in), volt_in[-5:])
-    print("Batt.   [V] : ", np.size(volt_bat), volt_bat[-5:])
-    print(" ")
-    print("Load    [%] : ", np.size(load_ups), load_ups[-5:])
-    print("Charge  [%] : ", np.size(charge_bat), charge_bat[-5:])
-    print(" ")
-    print("Runtime ['] : ", np.size(runtime_bat), runtime_bat[-5:])
-    --- End debugging.
-    """
     # Set the bar width
-    bar_width = 0.75
-    # positions of the left bar-boundaries
-    tick_pos = list(range(1, len(data_lbls) + 1))
+    # bar_width = 0.75
     fig_x = 10
     fig_y = 2.5
     fig_fontsize = 6.5
+    ahpla = 0.6
 
-    # *** VOLTAGE / LOAD
+    """
+    # ###############################
+    # Create a line plot of load and line voltage
+    # ###############################
+    """
     plt.rc('font', size=fig_fontsize)
-    dummy, ax1 = plt.subplots(1, figsize=(fig_x, fig_y))
-
-    """
-    # ###############################
-    # Create a line plot of volt_in/load_ups
-    # ###############################
-    """
-
-    # LOAD
-    ahpla = 0.4
-    ax1.errorbar(tick_pos, load_ups,
-                 yerr=[load_ups - load_ups_min, load_ups_max - load_ups],
-                 elinewidth=10,
-                 label='Load',
-                 alpha=ahpla,
-                 color='blue',
-                 marker='o'
-                 )
+    ax1 = data_frame.plot(x='sample_epoch',
+                          y=['load_ups', 'volt_in'],
+                          kind='line',
+                          figsize=(fig_x, fig_y),
+                          style=['b-', 'r-'],
+                          secondary_y=['volt_in']
+                          )
+    lws = [1]
+    lwsr = [1]
+    alp = [ahpla]
+    alpr = [ahpla]
+    for i, l in enumerate(ax1.lines):
+        plt.setp(l, alpha=alp[i], linewidth=lws[i])
+    for i, l in enumerate(ax1.right_ax.lines):
+        plt.setp(l, alpha=alpr[i], linewidth=lwsr[i])
+    ax1.set_ylim(y_ax_limits(data_frame['load_ups'], 2))
+    ax1.right_ax.set_ylim(y_ax_limits(data_frame['volt_in'], 20))
     ax1.set_ylabel("[%]")
-    ax1.legend(loc='upper left', framealpha=0.2)
-    # y_lo = int(min(load_ups) / 5) * 5
-    # y_hi = int(max(load_ups) / 5) * 5 + 5
-    # ax1.set_ylim([y_lo, y_hi])
-
-    # Set general plotting stuff
-    ax1.set_xlabel("Datetime")
+    ax1.right_ax.set_ylabel("[V]")
+    ax1.legend(loc='upper left', framealpha=0.2, labels=['load'])
+    ax1.right_ax.legend(loc='upper right', framealpha=0.2, labels=['line'])
+    # ax1.set_xlabel("Datetime")
     ax1.grid(which='major', axis='y', color='k', linestyle='--', linewidth=0.5)
-    plt.xticks(tick_pos, data_lbls, rotation=-60)
-
-    ax2 = ax1.twinx()
-
-    # volt_in
-    ahpla = 0.7
-    ax2.errorbar(tick_pos, volt_in,
-                 yerr=[volt_in - volt_in_min, volt_in_max - volt_in],
-                 elinewidth=5,
-                 label='Line',
-                 alpha=ahpla,
-                 color='red',
-                 marker='o'
-                 )
-    ax2.set_ylabel("[V]")
-    ax2.legend(loc='upper right', framealpha=0.2)
-    # y_lo = min(int(min(volt_in)) - 1, int(min(volt_bat)) - 1)
-    # y_hi = max(int(max(volt_in)) + 1, int(max(volt_bat)) + 1)
-    # if y_lo > 19:
-    #     y_lo = 19
-    # if y_hi < 22:
-    #     y_hi = 22
-    ax2.set_ylim([200, 250])
-
-    # Fit everything nicely
-    plt.title(f'{plot_title}')
-    plt.xlim([min(tick_pos) - bar_width, max(tick_pos) + bar_width])
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.savefig(fname=f'{output_file}V.png', format='png')
 
-    # *** BATTERY CHARGE
+    """
+    # ###############################
+    # Create a line plot of runtime
+    # ###############################
+    """
     plt.rc('font', size=fig_fontsize)
-    dummy, ax3 = plt.subplots(1, figsize=(fig_x, fig_y))
+    ax1 = data_frame.plot(x='sample_epoch',
+                          y=['runtime_bat'],
+                          kind='line',
+                          figsize=(fig_x, fig_y),
+                          style=['g']
+                          )
+    lws = [4]
+    alp = [ahpla]
+    for i, l in enumerate(ax1.lines):
+        plt.setp(l, alpha=alp[i], linewidth=lws[i])
+    ax1.set_ylim(y_ax_limits(data_frame['runtime_bat'], 50))
+    ax1.set_ylabel("[min]")
+    ax1.legend(loc='upper left', framealpha=0.2, labels=['runtime'])
+    # ax1.set_xlabel("Datetime")
+    ax1.grid(which='major', axis='y', color='k', linestyle='--', linewidth=0.5)
+    # plt.tight_layout()
+    plt.savefig(fname=f'{output_file}RUN.png', format='png')
+
     """
     # ###############################
-    # Create a bar plot of charge_bat
+    # Create a line plot of charge
     # ###############################
     """
-    ax3.bar(tick_pos, charge_bat_min,
-            width=bar_width,
-            label='Charge (min)',
-            alpha=ahpla,
-            color='lightgreen',
-            align='center',
-            bottom=0
-            )
-    ax3.bar(tick_pos, charge_bat_max - charge_bat_min,
-            width=bar_width,
-            label='Charge (max)',
-            alpha=ahpla,
-            color='green',
-            align='center',
-            bottom=charge_bat_min
-            )
-
-    # Set Axes stuff
-    ax3.set_ylabel("[%]")
-    ax3.set_xlabel("Datetime")
-    ax3.grid(which='major', axis='y', color='k', linestyle='--', linewidth=0.5)
-    ax3.legend(loc='upper left', framealpha=0.2)
-
-    # Set plot stuff
-    plt.xticks(tick_pos, data_lbls, rotation=-60)
-    # plt.title(f'{plot_title}')
-
-    # Fit every nicely
-    plt.xlim([min(tick_pos) - bar_width, max(tick_pos) + bar_width])
-    # y_lo = min(charge_bat_min) - 2
-    # y_hi = max(charge_bat_max) + 2
-    # if y_lo > 990:
-    #     y_lo = 990
-    # if y_hi < 1020:
-    #     y_hi = 1020
-    # ax3.set_ylim([y_lo, y_hi])
-    plt.tight_layout()
+    plt.rc('font', size=fig_fontsize)
+    ax1 = data_frame.plot(x='sample_epoch',
+                          y=['charge_bat'],
+                          kind='line',
+                          figsize=(fig_x, fig_y),
+                          style=['brown']
+                          )
+    lws = [4]
+    alp = [ahpla]
+    for i, l in enumerate(ax1.lines):
+        plt.setp(l, alpha=alp[i], linewidth=lws[i])
+    ax1.set_ylim(y_ax_limits(data_frame['charge_bat'], 50))
+    ax1.set_ylabel("[%]")
+    ax1.legend(loc='upper left', framealpha=0.2, labels=['charge'])
+    # ax1.set_xlabel("Datetime")
+    ax1.grid(which='major', axis='y', color='k', linestyle='--', linewidth=0.5)
+    # plt.tight_layout()
     plt.savefig(fname=f'{output_file}CHG.png', format='png')
 
-    # *** TOTAL VOC
-    plt.rc('font', size=fig_fontsize)
-    dummy, ax4 = plt.subplots(1, figsize=(fig_x, fig_y))
-    """
-    # ###############################
-    # Create a bar plot of total VOCs
-    # ###############################
-    """
-    ax4.bar(tick_pos, runtime_bat_min,
-            width=bar_width,
-            label='runtime (min)',
-            alpha=ahpla,
-            color='magenta',
-            align='center',
-            bottom=0
-            )
-    ax4.bar(tick_pos, runtime_bat_max - runtime_bat_min,
-            width=bar_width,
-            label='runtime (max)',
-            alpha=ahpla,
-            color='purple',
-            align='center',
-            bottom=runtime_bat_min
-            )
-
-    # Set Axes stuff
-    ax4.set_ylabel("runtime [min]")
-    ax4.set_xlabel("Datetime")
-    ax4.grid(which='major', axis='y', color='k', linestyle='--', linewidth=0.5)
-    ax4.legend(loc='upper left', framealpha=0.2)
-
-    # Set plot stuff
-    plt.xticks(tick_pos, data_lbls, rotation=-60)
-    # plt.title(f'{plot_title}')
-
-    # Fit every nicely
-    plt.xlim([min(tick_pos) - bar_width, max(tick_pos) + bar_width])
-    # y_lo = min(runtime_bat) - 10
-    # y_hi = max(runtime_bat) + 10
-    # if y_lo > 0:
-    #     y_lo = 0
-    # if y_hi < 1000:
-    #     y_hi = int(y_hi / 100) * 100 + 100
-    # ax4.set_ylim([y_lo, y_hi])
-    plt.tight_layout()
-    plt.savefig(fname=f'{output_file}RUN.png', format='png')
 
 
 def main():
@@ -284,12 +181,12 @@ def main():
     if OPTION.hours:
         plot_graph(f'/tmp/{MYAPP}/site/img/pastday_',
                    fetch_last_day(OPTION.hours),
-                   f"Trend afgelopen dagen ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})"
+                   f"Trend afgelopen uren ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})"
                    )
     if OPTION.days:
         plot_graph(f'/tmp/{MYAPP}/site/img/pastmonth_',
                    fetch_last_day(OPTION.days * 24),
-                   f"Trend afgelopen maand ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})"
+                   f"Trend afgelopen dagen ({dt.now().strftime('%d-%m-%Y %H:%M:%S')})"
                    )
 
 

c909e867924e0172c0e846e58c8f49621da1ddfe
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-01-15 08:46:38 +0100

    standardised script

--- bin/constants.sh
+++ bin/constants.sh
@@ -3,7 +3,8 @@ app_name="upsdiagd"
 # determine controller's identity
 host_name=$(hostname)
 
+
 # construct database paths
 database_filename="upsdata.sqlite3"
-# local_db_path="${HOME}/.sqlite3"
-# database_path="/srv/data"
+database_path="/srv/databases"
+db_full_path="${database_path}/${database_filename}"
